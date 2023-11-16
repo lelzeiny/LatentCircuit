687611714ab321491e01d439455962125a9a008f
Leena E <lelzeiny@berkeley.edu>
2023-11-16 10:55:53 -0800

    mini sanity checks pickle files

--- mini_tests/design1.v
+++
@@ -1,6 +0,0 @@
-module t(input [3: 0] i1, input [3: 0] i2, output [3: 0] o);
- assign o[0] = ^i1[3:1] << !i1[3] - i1[0] ;
- assign o[1] = ~^i1[3:2] * !i1[3] >> !i1[3] ;
- assign o[2] = ~|i1[1:0] << !i2[1] - !i1[2] ;
- assign o[3] = ^i2[3:1] - i1[1] / !i1[2] ;
- endmodule
--- mini_tests/design3.v
+++
@@ -1,6 +0,0 @@
-module t(input [3: 0] i1, input [3: 0] i2, output [3: 0] o);
- assign o[0] = &i2[1:0] >> i1[0] + !i1[3] ;
- assign o[1] = ~&i1[1:0] >> !i1[0] + i1[1] ;
- assign o[2] = &i2[3:0] && !i2[0] || !i2[0] ;
- assign o[3] = ^i1[3:1] && i1[3] && !i1[1] ;
- endmodule
